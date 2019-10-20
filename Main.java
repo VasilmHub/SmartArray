b1a16a40e720668ccfffae773d00f4bb7a1763b3
VasilmHub <48993358+VasilmHub@users.noreply.github.com>
2019-10-20 21:46:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package smartarray;
+
+public class Main {
+    public static void main(String[] args) {
+        SmartArray smartArray = new SmartArray();
+        for (int i = 0; i < 50; i++) {
+            smartArray.add(i);
+        }
+
+        smartArray.replace(-1, 3);
+        for (int i = 0; i <smartArray.size() ; i++) {
+            System.out.println(smartArray.get(i));
+        }
+
+    }
+}
---
+++ SmartArray.java
@@ -0,0 +1,86 @@
+package smartarray;
+
+public class SmartArray {
+    private static final int INITIAL_CAPACITY = 4;
+    private int[] date;
+    private int size;
+    int capacity;
+
+    public SmartArray() {
+        this.date = new int[INITIAL_CAPACITY];
+        this.capacity = INITIAL_CAPACITY;
+        size = 0;
+    }
+
+    private void shrink() {
+        if (this.capacity / size == 4) {
+            this.capacity /= 2;
+        }
+    }
+
+    public boolean contains(int number) {
+        for (int i = 0; i < size; i++) {
+            if (date[i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void remove(int index) {
+        CheckIndex(index);
+
+        ShiftLeft(index);
+        size--;
+        shrink();
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    private void ShiftLeft(int index) {
+        for (int i = index; i < size; i++) {
+            this.date[i] = this.date[i + 1];
+        }
+        this.date[size - 1] = 0;
+    }
+    public void replace(int index1, int index2){
+        CheckIndex(index1);
+        CheckIndex(index2);
+        int temp=this.date[index1];
+        this.date[index1]=this.date[index2];
+        this.date[index2]=temp;
+    }
+
+    public void add(int number) {
+        if (this.size == this.capacity) {
+            resize();
+        }
+        date[size] = number;
+        this.size++;
+    }
+
+    public int get(int index) {
+        this.CheckIndex(index);
+        return date[index];
+    }
+
+    private void resize() {
+        this.capacity = capacity * 2;
+        int[] temp = new int[this.capacity];
+        for (int i = 0; i < this.capacity / 2; i++) {
+            temp[i] = this.date[i];
+        }
+        this.date = new int[this.capacity];
+        this.date = temp;
+
+    }
+
+    private boolean CheckIndex(int index) {
+        if (index >= 0 && index < this.size) {
+            return true;
+        }
+        throw new IndexOutOfBoundsException("Error index");
+    }
+}
